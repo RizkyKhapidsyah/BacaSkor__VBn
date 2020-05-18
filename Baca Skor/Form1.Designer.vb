a1c61a4ccdcb52b6745d19400e963e0d1b9f5435
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-18 23:38:53 +0700

    Add project files.

---
+++ Baca Skor/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.labelRerata = New System.Windows.Forms.Label()
+        Me.tombolBaca = New System.Windows.Forms.Button()
+        Me.tombolKeluar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Rerata Ujian:"
+        '
+        'labelRerata
+        '
+        Me.labelRerata.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelRerata.Location = New System.Drawing.Point(89, 21)
+        Me.labelRerata.Name = "labelRerata"
+        Me.labelRerata.Size = New System.Drawing.Size(183, 23)
+        Me.labelRerata.TabIndex = 1
+        '
+        'tombolBaca
+        '
+        Me.tombolBaca.Location = New System.Drawing.Point(16, 72)
+        Me.tombolBaca.Name = "tombolBaca"
+        Me.tombolBaca.Size = New System.Drawing.Size(114, 30)
+        Me.tombolBaca.TabIndex = 2
+        Me.tombolBaca.Text = "Baca Skor"
+        Me.tombolBaca.UseVisualStyleBackColor = True
+        '
+        'tombolKeluar
+        '
+        Me.tombolKeluar.Location = New System.Drawing.Point(158, 72)
+        Me.tombolKeluar.Name = "tombolKeluar"
+        Me.tombolKeluar.Size = New System.Drawing.Size(114, 30)
+        Me.tombolKeluar.TabIndex = 3
+        Me.tombolKeluar.Text = "Keluar"
+        Me.tombolKeluar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 121)
+        Me.Controls.Add(Me.tombolKeluar)
+        Me.Controls.Add(Me.tombolBaca)
+        Me.Controls.Add(Me.labelRerata)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Skor Ujian"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents labelRerata As System.Windows.Forms.Label
+    Friend WithEvents tombolBaca As System.Windows.Forms.Button
+    Friend WithEvents tombolKeluar As System.Windows.Forms.Button
+
+End Class
---
+++ Baca Skor/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+
+    Private Sub tombolBaca_Click(sender As Object, e As EventArgs) Handles tombolBaca.Click
+        'prosedur ini membaca skor ujian, kemudian menghitung dan
+        'menampilkan rerata
+        Dim intBanyakSkor As Integer    'banyak skor ujian
+        Dim dblSkorUjian As Double      'memuat skor ujian
+        Dim dblTotal As Double = 0      'akumulator, diinisialisasi dengan 0
+        Dim dblRerata As Double         'rerata dari skor-skor ujian
+        Dim strMasukan As String        'memuat masukan user
+        Dim intHitung As Integer = 1    'variabel pencacah, diinisialisasi dengan 1
+
+        'meminta user memasukkan banyak skor ujian
+        strMasukan = InputBox("Berapa banyak skor ujian yang ingin direratakan?")
+
+        'mengkonversi masukan menjadi sebuah integer
+        If Integer.TryParse(strMasukan, intBanyakSkor) Then
+
+            'membaca skor-skor ujian
+            Do Until intHitung > intBanyakSkor
+                'meminta user untuk memasukkan skor ujian
+                strMasukan = InputBox("Masukkan skor ujian " &
+                intHitung.ToString())
+
+                'mengkonversi masukan menjadi sebuah Double
+                If Double.TryParse(strMasukan, dblSkorUjian) Then
+
+                    'menambahkan skor pada akumulator
+                    dblTotal += dblSkorUjian
+
+                    'menambah 1 pada pencacah
+                    intHitung += 1
+                Else
+                    'skor ujian tak-valid
+                    MessageBox.Show("Masukkan skor ujian numerik.")
+                End If
+            Loop
+
+            'menghitung dan menampilkan rerata
+            If intBanyakSkor > 0 Then
+                dblRerata = dblTotal / intBanyakSkor
+                labelRerata.Text = dblRerata.ToString()
+            End If
+
+        Else
+            'nilai tak valid dari banyak skor ujian
+            MessageBox.Show("Masukkan nilai integer untuk banyak skor ujian.")
+        End If
+    End Sub
+
+    Private Sub tombolKeluar_Click(sender As Object, e As EventArgs) Handles tombolKeluar.Click
+        'menutup form
+        Me.Close()
+    End Sub
+End Class
